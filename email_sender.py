97baf50d880793068caf451041de014c9b63fa9a
Kaan Buke <kabuke13@gmail.com>
2025-08-02 16:14:00 +0200

    allows for more dynamic emailing

--- email_sender.py
+++ email_sender.py
@@ -1,7 +1,13 @@
 import smtplib # creates an smtp server that allows emails
 from email.message import EmailMessage # Python's built in 
+
 from dotenv import load_dotenv
 import os
+
+from string import Template # takes a single argument which is a template string
+from pathlib import Path # similar to os.path
+
+html = Template(Path("index.html").read_text()) # now it is a template object
 
 email = EmailMessage() # this is our email object
 
@@ -14,7 +20,8 @@ email["from"] = "Kaan Buke"
 email["to"] = "kbuke1301@gmail.com"
 email["subject"] = "You won $1,000,000!!!!"
 
-email.set_content("I am a Python Master") # what's the content of email
+# email.set_content("I am a Python Master") # what's the content of email
+email.set_content(html.substitute({"name": "TinTin"}), 'html')
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp: #this is specific for email providers
     smtp.ehlo() # part of protocal of smtp
